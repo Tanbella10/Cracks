0844c9d5a63613dddbda9c329d63a9f096eb450b
Stepha <taesernag@correo.udistrital.edu.co>
2024-03-05 23:23:41 -0500

    para 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Cracks/",
   plugins: [react()],
-  
+/*   base: "https://Tanbella10.github.io/Cracks" */
 })
